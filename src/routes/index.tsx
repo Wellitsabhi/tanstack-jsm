be8f6c0e03b99a07f9f28db6e24f1accad219d9d
Abhishek Singh <abhishekongit@gmail.com>
2026-04-20 19:02:25 +0530

    data fetching

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,52 @@
-import SkillCard from '#/components/SkillCard'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, notFound, useRouter } from "@tanstack/react-router";
+import SkillCard from "#/components/SkillCard";
 
-export const Route = createFileRoute('/')({ component: App })
+const POKE_API = "https://pokeapi.co/api/v2/pokemon";
+
+export const Route = createFileRoute("/")({
+	component: App,
+	pendingComponent: () => <div className="p-14 text-center">Loading data</div>,
+	pendingMs: 300,
+	loader: async () => {
+		const res = await fetch(POKE_API);
+		// throw notFound( );
+		// throw new Error("khel khatam");
+		const data = await res.json();
+		return data;
+	},
+	errorComponent: ({ error }) => {
+		const router = useRouter();
+		return (
+			<div className="p-14 text-red-500">
+				<p>Some eror appeared!!! {error.message}</p>
+				<button onClick={() => router.invalidate()} type="button">
+					Try Again
+				</button>
+			</div>
+		);
+	},
+  notFoundComponent: () => <div className="p-12">Not Found</div>,
+});
 
 function App() {
-  return (
-    <main className="page-wrap px-4 pb-8 pt-14">
-      <h2> hi.  world</h2>
-     
-     <ul className='mt-7 list-none p-0 space-y-5'>
+	const data = Route.useLoaderData();
+	return (
+		<main className="page-wrap px-4 pb-8 pt-14">
+			<h2> hi. world</h2>
+
+			<ul className="mt-7 list-none p-0 space-y-5">
+				{data.results.map((pokemon: { name: string }) => (
+					<li key={pokemon.name}>
+						<SkillCard name={pokemon.name} />
+					</li>
+				))}
+			</ul>
+
+			{/* <ul className='mt-7 list-none p-0 space-y-5'>
       <li><SkillCard name="React" /></li>
       <li><SkillCard name="TansStack" /></li>
       <li><SkillCard name="Nodejs" /></li>
-     </ul>
-    </main>
-  )
+     </ul> */}
+		</main>
+	);
 }
